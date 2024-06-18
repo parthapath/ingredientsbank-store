b59b11d5d38af7d5588b394790152438476ac38d
Partha Saikia <partha@fuzeindia.com>
2024-06-19 00:06:55 +0530

    S3 URL

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["ingredientsbankpublic.s3.ap-south-1.amazonaws.com"],
+    domains: [process.env.S3_PUBLIC_URL],
   },
 };
 
