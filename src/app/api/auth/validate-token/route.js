d95485a2e06aa76194fd210de41d9615aef6fe19
Partha Saikia <partha@fuzeindia.com>
2024-06-20 12:48:05 +0530

    test

--- src/app/api/auth/validate-token/route.js
+++ src/app/api/auth/validate-token/route.js
@@ -6,13 +6,16 @@ export async function GET() {
   const token = cookies().get("token");
 
   if (!token) {
+    console.log("a");
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
   }
 
   try {
     jwt.verify(token.value, process.env.JWT_SECRET);
+    console.log("b");
     return NextResponse.json({ message: "Authorized" });
   } catch (err) {
+    console.log("c");
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
   }
 }
