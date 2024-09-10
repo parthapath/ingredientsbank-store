0b173a1c3be2b6de1713ea1da72a9021ff837ef5
Partha Saikia <partha@fuzeindia.com>
2024-09-10 12:30:00 +0530

    Terms and Conditions

--- src/app/shipping-and-returns/page.js
+++ src/app/shipping-and-returns/page.js
@@ -8,153 +8,35 @@ const shippingAndReturns = async () => {
   return (
     <div className={["page-wrapper", styles.ShippingAndReturns].join(" ")}>
       <div className="container">
-        <h2>Shipping &amp; Returns</h2>
-        <h3>Shipping Policy</h3>
-        <p>
-          <strong>Ingredients Bank</strong> is committed to delivering your
-          orders promptly and efficiently. Please read our shipping policy
-          carefully to understand our shipping procedures, timelines, and costs.
-        </p>
-        <h4>Shipping Methods and Costs</h4>
+        <h2>Shipping and Return Policy</h2>
         <ul>
           <li>
-            <strong>Standard Shipping</strong>: Estimated delivery time is 5-7
-            business days. Shipping costs will be calculated at checkout based
-            on the weight and destination of your order.
+            Ingredient must be approved or rejected with in 15-20 days from the
+            date of receipt at your warehouse / factory.
           </li>
           <li>
-            <strong>Expedited Shipping</strong>: Estimated delivery time is 2-3
-            business days. Additional charges apply and will be calculated at
-            checkout.
+            In case of Third party lab test , you must declare our batch no on
+            the external lab report and test method followed by buyer & seller
+            must be same. If difference in the teste method , returns are not
+            accepted incase of of rejection.
           </li>
           <li>
-            <strong>International Shipping</strong>: Delivery times and costs
-            vary by destination. International customers are responsible for any
-            customs duties or taxes imposed by their country.
+            If there is a rejections, goods must be returned with original
+            packaging and label photographs prior to return. returns will not be
+            accepted without email confirmation
+          </li>
+          <li>
+            Replacement for the return shipment is accepted with valid reason
+            for rejection and return and will be decided based on the shipping
+            terms and email confirmation.
+          </li>
+          <li>
+            <strong>Damages:</strong> Any damages must be brought to the
+            attention of Ingredients Bank within 2 days of the customer's
+            receipt. Proof of damage must be provided by photo or indicated on
+            the bill of lading when delivered.
           </li>
         </ul>
-        <h4>Processing Time</h4>
-        <ul>
-          <li>
-            Orders are processed within 1-2 business days after payment
-            confirmation.
-          </li>
-          <li>
-            Orders placed on weekends or holidays will be processed the next
-            business day.
-          </li>
-        </ul>
-        <h4>Order Tracking</h4>
-        <ul>
-          <li>
-            Once your order is shipped, you will receive an email confirmation
-            with a tracking number.
-          </li>
-          <li>
-            You can track your order using the tracking number provided on the
-            carrier&apos;s website.
-          </li>
-        </ul>
-        <h4>Shipping Restrictions</h4>
-        <ul>
-          <li>We do not ship to P.O. Boxes or APO/FPO addresses.</li>
-          <li>
-            Certain products may have shipping restrictions due to regulatory
-            requirements or manufacturer policies.
-          </li>
-        </ul>
-        <h3>Returns Policy</h3>
-        <p>
-          At <strong>Ingredients Bank</strong>, we strive to ensure your
-          complete satisfaction with our products. If you are not entirely
-          satisfied with your purchase, we are here to help.
-        </p>
-        <h4>Return Eligibility</h4>
-        <ul>
-          <li>Items must be returned within 30 days of receipt.</li>
-          <li>
-            Items must be unused, in their original packaging, and in the same
-            condition as when you received them.
-          </li>
-          <li>
-            Certain items, such as perishable goods and custom orders, are
-            non-returnable.
-          </li>
-        </ul>
-        <h4>Return Process</h4>
-        <ol>
-          <li>
-            <strong>Request a Return</strong>: Contact our customer service at
-            <a rel="noreferrer">support@ingredientsbank.com</a> to request a
-            return authorization.
-          </li>
-          <li>
-            <strong>Prepare Your Return</strong>: Include the original receipt
-            or proof of purchase with your return package.
-          </li>
-          <li>
-            <strong>Ship Your Return</strong>: Send your return to the following
-            address:
-          </li>
-          <li>
-            <strong>Return Shipping Costs</strong>: Customers are responsible
-            for return shipping costs unless the item is damaged or defective.
-          </li>
-        </ol>
-        <h4>Refunds</h4>
-        <ul>
-          <li>
-            Once we receive your returned item, we will inspect it and notify
-            you of the status of your refund.
-          </li>
-          <li>
-            If approved, your refund will be processed to your original method
-            of payment within 5-7 business days.
-          </li>
-          <li>
-            Shipping costs are non-refundable, except in cases of damaged or
-            defective items.
-          </li>
-        </ul>
-        <h4>Exchanges</h4>
-        <ul>
-          <li>We only replace items if they are defective or damaged.</li>
-          <li>
-            If you need to exchange an item for the same product, please contact
-            our customer service at{" "}
-            <a rel="noreferrer">support@ingredientsbank.com</a>.
-          </li>
-        </ul>
-        <h3>Damaged or Defective Items</h3>
-        <ul>
-          <li>
-            If you receive a damaged or defective item, please contact us
-            immediately at <a rel="noreferrer">support@ingredientsbank.com</a>{" "}
-            with your order number and details of the product&apos;s condition.
-          </li>
-          <li>
-            We will provide instructions for returning the damaged or defective
-            item and will cover return shipping costs.
-          </li>
-        </ul>
-        <h3>Contact Us</h3>
-        <p>
-          If you have any questions about our Shipping &amp; Returns policy,
-          please contact us at:
-        </p>
-        <p>
-          <strong>Email</strong>:{" "}
-          <a rel="noreferrer">support@ingredientsbank.com</a>
-          <br />
-          <strong>Phone</strong>: [Your Customer Service Phone Number]
-          <br />
-          <strong>Address</strong>: Ingredients Bank, [Your Business Address]
-        </p>
-        <p>
-          Thank you for shopping with <strong>Ingredients Bank</strong>. We
-          appreciate your business and are dedicated to providing you with the
-          best possible service.
-        </p>
       </div>
     </div>
   );
--- src/app/terms-and-conditions/page.js
+++ src/app/terms-and-conditions/page.js
@@ -9,174 +9,40 @@ const termsAndConditions = async () => {
     <div className={["page-wrapper", styles.TermsAndConditions].join(" ")}>
       <div className="container">
         <h2>Terms and Conditions</h2>
-        <h3>1. Introduction</h3>
+        <h3>1. Order Confirmation</h3>
+        <p>Final order confirmation will be based on email communication.</p>
+        <h3>2. Payments</h3>
         <p>
-          Welcome to www.ingredientsbank.com. These terms and conditions outline
-          the rules and regulations for the use of our website, located at
-          www.ingredientsbank.com.
+          Payments should be made to the advised bank details in the proforma
+          invoice. Only bank transfers are accepted; there are no card options.
         </p>
+        <h3>3. Pricing</h3>
         <p>
-          By accessing this website, we assume you accept these terms and
-          conditions. Do not continue to use www.ingredientsbank.com if you do
-          not agree to all of the terms and conditions stated on this page.
+          All prices are fixed as of the date of the invoice and/or the order
+          acknowledgment date. All prices quoted are exclusive of taxes, fees,
+          levies, duties, handling, and freight charges ("Taxes and Shipping
+          Charges") unless otherwise approved.
         </p>
-        <h3>2. Definitions</h3>
-        <ul>
-          <li>
-            <strong>Company</strong>: Refers to Ingredients Bank, the provider
-            of the services.
-          </li>
-          <li>
-            <strong>User</strong>: Refers to any individual or entity accessing
-            our website.
-          </li>
-          <li>
-            <strong>Products</strong>: Refers to the items available for sale on
-            our website.
-          </li>
-          <li>
-            <strong>Services</strong>: Refers to the e-commerce services
-            provided by Ingredients Bank.
-          </li>
-        </ul>
-        <h3>3. Use of Website</h3>
-        <ul>
-          <li>
-            Users must be at least 18 years old or accessing the website under
-            the supervision of a parent or legal guardian.
-          </li>
-          <li>
-            Users are granted a non-transferable, revocable license to use the
-            website for shopping for personal items sold on the site.
-          </li>
-          <li>
-            Commercial use or use on behalf of any third party is prohibited,
-            except as explicitly permitted by Ingredients Bank.
-          </li>
-        </ul>
-        <h3>4. Account and Registration</h3>
-        <ul>
-          <li>
-            Users must create an account to use certain features of the website.
-          </li>
-          <li>
-            Users agree to provide accurate and complete information and to
-            update it as necessary.
-          </li>
-          <li>
-            Users are responsible for maintaining the confidentiality of their
-            account information and for all activities that occur under their
-            account.
-          </li>
-        </ul>
-        <h3>5. Orders and Payment</h3>
-        <ul>
-          <li>All orders are subject to acceptance and availability.</li>
-          <li>
-            Ingredients Bank reserves the right to refuse or cancel any order
-            for any reason.
-          </li>
-          <li>Payment must be made in full before products are shipped.</li>
-        </ul>
-        <h3>6. Pricing and Availability</h3>
-        <ul>
-          <li>Prices for products are subject to change without notice.</li>
-          <li>
-            Ingredients Bank strives to ensure the accuracy of all information
-            but does not warrant that product descriptions or other content is
-            accurate, complete, or error-free.
-          </li>
-        </ul>
-        <h3>7. Shipping and Delivery</h3>
-        <ul>
-          <li>Shipping and delivery times are estimates and not guaranteed.</li>
-          <li>
-            Ingredients Bank is not responsible for delays caused by carriers or
-            customs clearance processes.
-          </li>
-          <li>
-            Shipping costs are non-refundable unless the product is damaged or
-            defective.
-          </li>
-        </ul>
-        <h3>8. Returns and Refunds</h3>
-        <ul>
-          <li>
-            Users may return products within [X] days of receipt for a refund or
-            exchange.
-          </li>
-          <li>
-            Products must be returned in their original condition and packaging.
-          </li>
-          <li>
-            Refunds will be processed within [X] days of receiving the returned
-            item.
-          </li>
-        </ul>
-        <h3>9. Intellectual Property</h3>
-        <ul>
-          <li>
-            All content on the website, including text, graphics, logos, images,
-            and software, is the property of Ingredients Bank and protected by
-            applicable copyright and trademark laws.
-          </li>
-          <li>
-            Users may not reproduce, distribute, or otherwise use any content
-            without explicit permission from Ingredients Bank.
-          </li>
-        </ul>
-        <h3>10. Limitation of Liability</h3>
-        <ul>
-          <li>
-            Ingredients Bank is not liable for any direct, indirect, incidental,
-            or consequential damages arising from the use or inability to use
-            the website.
-          </li>
-          <li>
-            This includes, but is not limited to, damages for loss of profits,
-            data, or other intangibles.
-          </li>
-        </ul>
-        <h3>11. Indemnification</h3>
-        <ul>
-          <li>
-            Users agree to indemnify and hold Ingredients Bank, its affiliates,
-            and employees harmless from any claim or demand, including
-            reasonable attorney fees, arising out of their use of the website or
-            violation of these terms.
-          </li>
-        </ul>
-        <h3>12. Governing Law</h3>
-        <ul>
-          <li>
-            These terms and conditions are governed by and construed in
-            accordance with the laws of [Your Country/State], without regard to
-            its conflict of law principles.
-          </li>
-          <li>
-            Any disputes arising from these terms will be resolved in the courts
-            of [Your Country/State].
-          </li>
-        </ul>
-        <h3>13. Changes to Terms</h3>
-        <ul>
-          <li>
-            Ingredients Bank reserves the right to modify these terms and
-            conditions at any time.
-          </li>
-          <li>
-            Users will be notified of any changes by posting the new terms on
-            the website. Continued use of the website after such changes
-            constitutes acceptance of the new terms.
-          </li>
-        </ul>
-        <h3>14. Contact Information</h3>
-        <ul>
-          <li>
-            For any questions or concerns about these terms and conditions,
-            please contact us at [Your Contact Information].
-          </li>
-        </ul>
+        <h3>4. Credit Terms</h3>
+        <p>Credit terms are agreed upon as per email confirmation.</p>
+        <h3>5. Shipping Terms</h3>
+        <p>
+          We support all shipping terms subject to email confirmation. Prices
+          may vary based on the terms.
+        </p>
+        <h3>6. Delivery Dates</h3>
+        <p>
+          All delivery dates are approximate and will be advised based on the
+          dispatch date.
+        </p>
+        <h3>7. Agreement</h3>
+        <p>
+          All order acknowledgments accepted by the buyer shall be subject to
+          these terms and conditions, which may be changed or amended from time
+          to time by Ingredients Bank. In the event of a discrepancy between the
+          terms in an invoice and these terms, the terms in the invoice shall
+          prevail.
+        </p>
         <hr />
         <p>
           By using our website, you acknowledge that you have read, understood,
